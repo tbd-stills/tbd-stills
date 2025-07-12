02393a8236f97f03de383aa16e00e19a2d2b175c
Trevor <trevor@hauter.dev>
2025-07-11 20:40:44 -0400

    Update behavior of fading text on home page

---
+++ front-of-house/src/components/FadingDiv.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { getWindowDimensions, useMousePosition } from '../hooks/hooks';
+
+import '../components/FadingDiv.css';
+
+function getPercentage(inverted: boolean, percent: number) {
+  if (inverted) {
+    return 1 - Math.round(percent * 100) / 100;
+  } else {
+    return Math.round(percent * 100) / 100;
+  }
+}
+
+export default function FadingDiv({ inverted = false, children }: { inverted: boolean, children: React.ReactNode }) {
+  const mousePosition = useMousePosition();
+  const dimensions = getWindowDimensions();
+
+  const windowWidthPercentage: number = mousePosition.x / dimensions.width;
+
+  const opacity: string = (
+    getPercentage(inverted, windowWidthPercentage)
+  ).toString();
+
+  return (
+    <div style={{ opacity: opacity }}>
+      { children }
+    </div>
+  );
+}
--- front-of-house/src/home/home.tsx
+++ front-of-house/src/home/home.tsx
@@ -1,21 +1,21 @@
-import FadingCover from '../components/fadingBackground';
+import FadingDiv from '../components/FadingDiv';
 
 import homeBackground from '../assets/homeBackground.jpg';
 import '../home/home.css';
 
 function Home() {
   return (
-    <>
-      <FadingCover src={homeBackground} />
-      <div
-        className="container"
-        style={{
-          backgroundColor: `rgb(32, 32, 32)`,
-        }}
-      >
-        <h1 style={{ color: 'white' }}>Coming soon...</h1>
+    <div style={{ height: '100%', width: '100%' }}>
+      <img className="cover" src={homeBackground} />;
+      <div className="home-container">
+        <FadingDiv inverted={false}>
+          <h1>TBD Stills</h1>
+        </FadingDiv>
+        <FadingDiv inverted={true}>
+          <h1>coming soon...</h1> 
+        </FadingDiv>
       </div>
-    </>
+    </div>
   );
 }
 
