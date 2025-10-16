b90ce180be07e4941db3f4c820ee06c4dd857aca
Trevor <trevor@hauter.dev>
2025-10-15 21:06:17 -0400

    Update quotes

--- front-of-house/src/constants.ts
+++ front-of-house/src/constants.ts
@@ -1,5 +1,5 @@
 export const homePagePhrases = [
-  'Made by humans, with love',
   'Art for arts sake',
   'A digital third space',
+  'But I myself cling to an ideal which I am able to create within my own imagination',
 ];
